b70e90cda2b3a2474e5c5cfd10689e0c5a6dda77
induwara-cmd <indendz0118@gmail.com>
2026-04-23 22:15:48 +0530

    feat: add request logging filter for API endpoints

--- smart-campus-api/src/main/java/com/smartcampus/api/filters/RequestLoggingFilter.java
+++ smart-campus-api/src/main/java/com/smartcampus/api/filters/RequestLoggingFilter.java
@@ -1,3 +1,19 @@
-public class RequestLoggingFilter {
-    
+package com.smartcampus.api.filters;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.ext.Provider;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Provider
+public final class RequestLoggingFilter implements ContainerRequestFilter {
+    private static final Logger logger = Logger.getLogger(RequestLoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) {
+        String method = requestContext.getMethod();
+        String uri = requestContext.getUriInfo().getRequestUri().toString();
+        logger.log(Level.INFO, "{0} {1}", new Object[] { method, uri });
+    }
 }
